1cc1280e0ac7e856a1b9d911eed300c7fcd7ae36
rezwanrahimr <rezwanrahim69@gmail.com>
2023-10-29 22:28:23 +0600

    add all api in backend

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,18 +1,5 @@
-
-
-
 function App() {
-
-
-  return (
-    <>
-
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-
-    </>
-  )
+  return <></>;
 }
 
-export default App
+export default App;
---
+++ FrontEnd/src/Layout/index.jsx
@@ -0,0 +1,11 @@
+import Navbar from "../Components/Navbar";
+
+const Layout = () => {
+  return (
+    <>
+      <Navbar></Navbar>
+    </>
+  );
+};
+
+export default Layout;
