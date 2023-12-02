3ee690e045216e5b688a5bbd4e5610cf16a35286
rezwanrahimr <rezwanrahim69@gmail.com>
2023-12-02 23:46:26 +0600

    Update project and Install redux and react_redux and redux thunk

--- FrontEnd/src/Components/AddForm/AddForm.jsx
+++ FrontEnd/src/Components/AddForm/AddForm.jsx
@@ -1,21 +1,45 @@
+import PostData from "../../redux/thunk/PostData";
+
 const AddForm = () => {
-    const handleSubmit = () =>{
-        
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const information = {
+      title: e.target.title.value,
+      description: e.target.description.value,
+    };
+
+    console.log("post info", information);
+
+    if (information) {
+      PostData(information);
     }
-    return <>
-
-        <h1 className="text-xl">Add New Post</h1>
-        <form  onSubmit={handleSubmit}>
-            <label>Title</label>
-            <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
-            <br />
-            <label > Description</label>
-            <br />
-            <textarea className="textarea textarea-bordered" placeholder="Bio"></textarea>
-            <br />
-            <button className="btn">Submit</button>
-
-        </form>
+  };
+  return (
+    <>
+      <h1 className="text-xl">Add New Post</h1>
+      <form onSubmit={handleSubmit}>
+        <label>Title</label>
+        <input
+          type="text"
+          placeholder="Type here"
+          className="input input-bordered w-full max-w-xs"
+          name="title"
+        />
+        <br />
+        <label> Description</label>
+        <br />
+        <textarea
+          className="textarea textarea-bordered"
+          placeholder="Bio"
+          name="description"
+        ></textarea>
+        <br />
+        <button className="btn" type="submit">
+          Submit
+        </button>
+      </form>
     </>
-}
+  );
+};
 export default AddForm;
---
+++ FrontEnd/src/redux/action/actionCreators.js
@@ -0,0 +1,20 @@
+import { FETCH_FAIL, FETCH_START, FETCH_SUCCESS } from "./actionTypes";
+
+export const fetchStart = () => {
+  return {
+    type: FETCH_START,
+  };
+};
+
+export const fetchSuccess = () => {
+  return {
+    type: FETCH_SUCCESS,
+  };
+};
+
+export const fetchFaild = (value) => {
+  return {
+    type: FETCH_FAIL,
+    payload: value,
+  };
+};
