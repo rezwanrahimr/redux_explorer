a43d5affc992e1a475ca9bfe6ab49c1e3cfd60df
rezwanrahimr <rezwanrahim69@gmail.com>
2023-12-03 16:12:18 +0600

    update

--- FrontEnd/src/Components/AddForm/AddForm.jsx
+++ FrontEnd/src/Components/AddForm/AddForm.jsx
@@ -1,41 +1,6 @@
 import PostData from "../../redux/thunk/PostData";
 
 const AddForm = () => {
-<<<<<<< HEAD
-
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        const formData = {
-            [name]: value
-        }
-
-        console.log("Handle CHange", formData)
-    }
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        const { name, value } = e.target;
-        // const formData = {
-        //     [name]: value
-        // }
-
-        console.log("FormData", name, value);
-    }
-    return <>
-
-        <h1 className="text-xl">Add New Post</h1>
-        <form onSubmit={handleSubmit}>
-            <label>Title</label>
-            <input onChange={handleChange} name="title" type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
-            <br />
-            <label > Description</label>
-            <br />
-            <textarea onChange={handleChange} name="description" className="textarea textarea-bordered" placeholder="Bio"></textarea>
-            <br />
-            <button className="btn" type="submit">Submit</button>
-
-        </form>
-=======
   const handleSubmit = (e) => {
     e.preventDefault();
 
@@ -74,7 +39,6 @@ const AddForm = () => {
           Submit
         </button>
       </form>
->>>>>>> 3ee690e045216e5b688a5bbd4e5610cf16a35286
     </>
   );
 };
