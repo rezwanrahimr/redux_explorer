cb63317dde8520438c6096e6cdf9d887f4ebac5d
rezwanrahimr <rezwanrahim69@gmail.com>
2023-12-03 16:02:25 +0600

    vercel config file added

--- FrontEnd/src/Components/AddForm/AddForm.jsx
+++ FrontEnd/src/Components/AddForm/AddForm.jsx
@@ -1,19 +1,35 @@
 const AddForm = () => {
-    const handleSubmit = () =>{
-        
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        const formData = {
+            [name]: value
+        }
+
+        console.log("Handle CHange", formData)
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const { name, value } = e.target;
+        // const formData = {
+        //     [name]: value
+        // }
+
+        console.log("FormData", name, value);
     }
     return <>
 
         <h1 className="text-xl">Add New Post</h1>
-        <form  onSubmit={handleSubmit}>
+        <form onSubmit={handleSubmit}>
             <label>Title</label>
-            <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
+            <input onChange={handleChange} name="title" type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
             <br />
             <label > Description</label>
             <br />
-            <textarea className="textarea textarea-bordered" placeholder="Bio"></textarea>
+            <textarea onChange={handleChange} name="description" className="textarea textarea-bordered" placeholder="Bio"></textarea>
             <br />
-            <button className="btn">Submit</button>
+            <button className="btn" type="submit">Submit</button>
 
         </form>
     </>
